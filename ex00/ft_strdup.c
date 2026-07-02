82687ff53279e9d9dadf850b2295202b9ad15dd7
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-02 13:29:28 +0200

    feat(ex00): add a function to allocate memory and copy a string

---
+++ ex00/ft_strdup.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/02 13:16:37 by nfurst            #+#    #+#             */
+/*   Updated: 2026/07/02 13:23:32 by nfurst           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	char	*ptr;
+
+	ptr = dest;
+	while (*src != '\0')
+	{
+		*ptr++ = *src++;
+	}
+	*ptr = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+
+	dest = malloc(ft_strlen(src) + 1);
+	ft_strcpy(dest, src);
+	return (dest);
+}
