199901b984206aeeae870b25406c9e8fc82a51bf
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-03 10:05:17 +0200

    refactor(ex02): comment out the main

--- ex02/ft_ultimate_range.c
+++ ex02/ft_ultimate_range.c
@@ -34,33 +34,32 @@ int	ft_ultimate_range(int **range, int min, int max)
 	return (i);
 }
 
-int main(int argc, char **argv)
-{
-    int min;
-    int max;
-    int *range;
-    int size;
-
-    if (argc != 3)
-        return (1);
-    min = atoi(argv[1]);
-    max = atoi(argv[2]);
-    size = ft_ultimate_range(&range, min, max);
-    if (size > 0)
-    {
-        for (int i = 0; i < size; i++)
-        {
-            printf("%d ", range[i]);
-        }
-        free(range);
-    }
-    else if (size == 0)
-    {
-        printf("Invalid range.\n");
-    }
-    else
-    {
-        printf("Memory allocation failed.\n");
-    }
-    return (0);
-}
+// int main(int argc, char **argv)
+// {
+//     int min;
+//     int max;
+//     int *range;
+//     int size;
+//     if (argc != 3)
+//         return (1);
+//     min = atoi(argv[1]);
+//     max = atoi(argv[2]);
+//     size = ft_ultimate_range(&range, min, max);
+//     if (size > 0)
+//     {
+//         for (int i = 0; i < size; i++)
+//         {
+//             printf("%d ", range[i]);
+//         }
+//         free(range);
+//     }
+//     else if (size == 0)
+//     {
+//         printf("Invalid range.\n");
+//     }
+//     else
+//     {
+//         printf("Memory allocation failed.\n");
+//     }
+//     return (0);
+// }
