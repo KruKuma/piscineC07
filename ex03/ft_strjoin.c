a9b5a0c3e5156dc82910345dfa6036255cef40c8
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-09 15:55:17 +0200

    fix(ex03): fix formatting for norminette

--- ex03/ft_strjoin.c
+++ ex03/ft_strjoin.c
@@ -6,74 +6,76 @@
 /*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/06 15:53:32 by nfurst            #+#    #+#             */
-/*   Updated: 2026/07/09 15:40:29 by nfurst           ###   ########.fr       */
+/*   Updated: 2026/07/09 15:54:32 by nfurst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 // #include <stdio.h>
 
-int    ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
-    int length = 0;
-    while (str[length] != '\0')
-    {
-        length++;
-    }
-    return length;
+	int	length;
+
+	length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
 }
 
-int    ft_total_length(int size, char **strs, char *sep)
+int	ft_total_length(int size, char **strs, char *sep)
 {
-    int i;
-    int len;
-    int sep_len;
+	int	i;
+	int	len;
+	int	sep_len;
 
-    if (size <= 0)
-        return 1;
-    i = 0;
-    len = 0;
-    sep_len = ft_strlen(sep);
-    while (i < size)
-    {
-        len += ft_strlen(strs[i]);
-        i++;
-    }
-    len += sep_len * (size - 1);
-    return (len + 1);
+	if (size <= 0)
+		return (1);
+	i = 0;
+	len = 0;
+	sep_len = ft_strlen(sep);
+	while (i < size)
+	{
+		len += ft_strlen(strs[i]);
+		i++;
+	}
+	len += sep_len * (size - 1);
+	return (len + 1);
 }
 
-void    ft_strcat(char *dest, char *src)
+void	ft_strcat(char *dest, char *src)
 {
-    while (*dest)
-        dest++;
-    while (*src)
-    {
-        *dest = *src;
-        dest++;
-        src++;
-    }
-    *dest = '\0';
+	while (*dest)
+		dest++;
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
 }
 
-char    *ft_strjoin(int size, char **strs, char *sep)
+char	*ft_strjoin(int size, char **strs, char *sep)
 {
-    char *result;
-    int i;
+	char	*result;
+	int		i;
 
-    result = malloc(sizeof(char) * ft_total_length(size, strs, sep));
-    if (!result)
-        return (0);
-    *result = '\0';
-    i = 0;
-    while (i < size)
-    {
-        ft_strcat(result, strs[i]);
-        if (i < size - 1)
-            ft_strcat(result, sep);
-        i++;
-    }
-    return (result);
+	result = malloc(sizeof(char) * ft_total_length(size, strs, sep));
+	if (!result)
+		return (0);
+	*result = '\0';
+	i = 0;
+	while (i < size)
+	{
+		ft_strcat(result, strs[i]);
+		if (i < size - 1)
+			ft_strcat(result, sep);
+		i++;
+	}
+	return (result);
 }
 
 // int main(int argc, char **argv)
