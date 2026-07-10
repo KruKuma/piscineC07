bdfbca7a3f3c82c1e11877685ed54beea3893771
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-10 14:37:34 +0200

    fix(ex01): fix main to print the range

--- ex01/ft_range.c
+++ ex01/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/02 19:42:12 by nfurst            #+#    #+#             */
-/*   Updated: 2026/07/03 10:00:08 by nfurst           ###   ########.fr       */
+/*   Updated: 2026/07/10 14:36:54 by nfurst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,23 +37,24 @@ int	*ft_range(int min, int max)
 // {
 //     int min;
 //     int max;
+// 	int size;
+// 	int i;
+
 //     if (argc != 3)
 //         return (1);
 //     min = atoi(argv[1]);
 //     max = atoi(argv[2]);
 //     int *range = ft_range(min, max);
-//     if (range)
-//     {
-//         while (min < max)
-//         {
-//             printf("%d ", range[min - min]);
-//             min++;
-//         }
-//         free(range);
-//     }
-//     else
-//     {
-//         printf("Invalid range or memory allocation failed.\n");
-//     }
+//     if (range == 0)
+// 		return (1);
+// 	size = max - min;
+// 	i = 0;
+// 	while (i < size)
+// 	{
+// 		printf("%d ", range[i]);
+// 		i++;
+// 	}
+// 	printf("\n");
+// 	free(range);
 //     return (0);
 // }
